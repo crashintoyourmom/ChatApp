79e3ea94cd4b1e1ee89095a03eed19d16d49bf26
My Name <me@you.com>
2020-08-14 15:58:26 +1200

    error page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,13 @@
 let expandsidebar = document.querySelector("#expand-sidebar");
 expandsidebar.addEventListener("click", (e) => {
-    if (expandsidebar.setAttribute("data-expanded", true)){
-        
+    if (expandsidebar.getAttribute("data-expanded") === "false") {
+        document.querySelector("#sidebar").classList.add("d-none");
+        document.querySelector("#main").classList.remove("d-none");
+        expandsidebar.setAttribute("data-expanded", "true");
+    } else {
+        document.querySelector("#sidebar").classList.remove("d-none");
+        document.querySelector("#main").classList.add("d-none");
+        expandsidebar.setAttribute("data-expanded", "false");
     }
-
-    document.querySelector("#main").classList.add("d-none");
-    document.querySelector("#sidebar").classList.remove("d-none");
-
-    document.querySelector("#sidebar").classList.add("d-none");
-    document.querySelector("#main").classList.remove("d-none");
+    
 })
